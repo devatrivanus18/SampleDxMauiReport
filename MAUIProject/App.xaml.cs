e800010d63d69f0f11a3ec3efede4a159580ede5
devazamili <38879571+devatrivanus18@users.noreply.github.com>
2022-12-14 16:29:40 +0700

    Add project files.

---
+++ MAUIProject/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUIProject;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MAUIProject/index.js
@@ -0,0 +1,42 @@
+﻿var ko = require('knockout');
+require('devexpress-reporting/dx-reportdesigner');
+import "./style.css";
+
+window.JsFunctions = {
+    _viewerOptions: {
+        reportUrl: ko.observable("SampleReport"),
+        requestOptions: {
+            host: "https://localhost:7261",
+            invokeAction: "/DXXRDV"
+        }
+    },
+    _designerOptions: {        
+        reportUrl: ko.observable("SampleReport"),
+        requestOptions: {
+            host: "https://localhost:7261",
+            getDesignerModelAction: "/DXXRD/GetReportDesignerModel"
+        }
+    },
+    _report1Options: {
+        reportUrl: ko.observable("Report1"),
+        requestOptions: {
+            host: "https://localhost:7261",
+            invokeAction: "/DXXRDV"
+        }
+    },
+    InitWebDocumentViewer: function () {
+        ko.applyBindings(this._viewerOptions, document.getElementById("viewer"));
+    },
+    InitWebReport1Viewer: function () {
+        ko.applyBindings(this._report1Options, document.getElementById("report1"));
+    },
+    InitReportDesigner: function () {
+        ko.applyBindings(this._designerOptions, document.getElementById("designer"));
+    },
+    Dispose: function (elementId) {
+        var element = document.getElementById(elementId);
+        element && ko.cleanNode(element);
+    }
+}
+
+
