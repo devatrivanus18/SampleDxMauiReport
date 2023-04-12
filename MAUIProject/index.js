e2b1adca9eb28943346671794939d2b186e0d758
mafifah <mafifah.usr@gmail.com>
2023-04-12 10:03:03 +0700

    Perbaikan

--- MAUIProject/index.js
+++ MAUIProject/index.js
@@ -5,32 +5,22 @@ window.JsFunctions = {
     _viewerOptions: {
         reportUrl: "",
         requestOptions: {
-            host: "https://localhost:7261",
+            host: "https://fctestreport.azurewebsites.net/",
             invokeAction: "/DXXRDV"
         }
     },
     _designerOptions: {        
         reportUrl: ko.observable("SampleReport"),
         requestOptions: {
-            host: "https://localhost:7261",
+            host: "https://fctestreport.azurewebsites.net/",
             getDesignerModelAction: "/DXXRD/GetReportDesignerModel"
         }
     },
-    //_report1Options: {
-    //    reportUrl: ko.observable("Report1"),
-    //    requestOptions: {
-    //        host: "https://localhost:7261",
-    //        invokeAction: "/DXXRDV"
-    //    }
-    //},
     InitWebDocumentViewer: function (elementId, namaReport) {
         this._viewerOptions.reportUrl = namaReport;
         console.log(this._viewerOptions.reportUrl);
         ko.applyBindings(this._viewerOptions, document.getElementById(elementId));
     },
-    //InitWebReport1Viewer: function () {
-    //    ko.applyBindings(this._report1Options, document.getElementById("report1"));
-    //},
     InitReportDesigner: function (elementId) {
         console.log(elementId);
         ko.applyBindings(this._designerOptions, document.getElementById(elementId));
